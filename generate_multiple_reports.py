e7975c79d790565c5db613e083c7f63b956b1f7f
David Shapiro <daveshap37@gmail.com>
2023-07-06 12:12:57 -0400

    all done

---
+++ generate_multiple_reports.py
@@ -0,0 +1,118 @@
+import openai
+from time import time, sleep
+from halo import Halo
+import textwrap
+import yaml
+import os
+import PyPDF2
+
+
+prompts = [
+'Can you give me a very clear explanation of the core assertions, implications, and mechanics elucidated in this paper?',
+
+"Can you explain the value of this in basic terms? Like you're talking to a CEO. So what? What's the bottom line here?",
+
+'Can you give me an analogy or metaphor that will help explain this to a broad audience.',
+]
+
+
+
+###     file operations
+
+
+def save_file(filepath, content):
+    with open(filepath, 'w', encoding='utf-8') as outfile:
+        outfile.write(content)
+
+
+
+def open_file(filepath):
+    with open(filepath, 'r', encoding='utf-8', errors='ignore') as infile:
+        return infile.read()
+
+
+def save_yaml(filepath, data):
+    with open(filepath, 'w', encoding='utf-8') as file:
+        yaml.dump(data, file, allow_unicode=True)
+
+
+def open_yaml(filepath):
+    with open(filepath, 'r', encoding='utf-8') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+    return data
+
+
+###     API functions
+
+
+def chatbot(conversation, model="gpt-4-0613", temperature=0):
+    max_retry = 7
+    retry = 0
+    while True:
+        try:
+            spinner = Halo(text='Thinking...', spinner='dots')
+            spinner.start()
+            
+            response = openai.ChatCompletion.create(model=model, messages=conversation, temperature=temperature)
+            text = response['choices'][0]['message']['content']
+
+            spinner.stop()
+            
+            return text, response['usage']['total_tokens']
+        except Exception as oops:
+            print(f'\n\nError communicating with OpenAI: "{oops}"')
+            if 'maximum context length' in str(oops):
+                a = conversation.pop(0)
+                print('\n\n DEBUG: Trimming oldest message')
+                continue
+            retry += 1
+            if retry >= max_retry:
+                print(f"\n\nExiting due to excessive errors in API: {oops}")
+                exit(1)
+            print(f'\n\nRetrying in {2 ** (retry - 1) * 5} seconds...')
+            sleep(2 ** (retry - 1) * 5)
+
+
+
+def chat_print(text):
+    formatted_lines = [textwrap.fill(line, width=120, initial_indent='    ', subsequent_indent='    ') for line in text.split('\n')]
+    formatted_text = '\n'.join(formatted_lines)
+    print('\n\n\nCHATBOT:\n\n%s' % formatted_text)
+
+
+
+
+if __name__ == '__main__':
+    # instantiate chatbot, variables
+    openai.api_key = open_file('key_openai.txt').strip()
+
+    # Get list of all PDF files in the input folder
+    pdf_files = [f for f in os.listdir('input/') if f.endswith('.pdf')]
+
+    for pdf_file in pdf_files:
+        # Check if the report already exists in the output folder
+        filename = 'output/' + pdf_file.replace('.pdf', '.txt')
+        if os.path.exists(filename):
+            continue
+
+        # Open the PDF file
+        with open('input/' + pdf_file, 'rb') as file:
+            pdf_reader = PyPDF2.PdfReader(file)
+            paper = ''
+            for page_num in list(range(0,len(pdf_reader.pages))):
+                page = pdf_reader.pages[page_num]
+                paper += page.extract_text()
+
+        if len(paper) > 22000:
+            paper = paper[0:22000]
+        ALL_MESSAGES = [{'role':'system', 'content': paper}]
+        report = ''
+        for p in prompts:
+            ALL_MESSAGES.append({'role':'user', 'content': p})
+            response, tokens = chatbot(ALL_MESSAGES)
+            chat_print(response)
+            ALL_MESSAGES.append({'role':'assistant', 'content': response})
+            report += '\n\n\n\nQ: %s\n\nA: %s' % (p, response)
+
+        # Save the report in the output folder with the same name as the PDF file but with .txt extension
+        save_file(filename, report.strip())
